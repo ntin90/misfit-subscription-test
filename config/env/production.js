9ebdfd2ed94ca72f024aebf4f5258150828fb139
ntin90 <nttin@misfit.com>
2017-03-13 22:52:43 +0700

    log debug

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-    log: { level: "info"},
+    log: { level: "debug"},
     self_host: process.env.DOMAIN,
     app_key: process.env.APP_KEY,
     app_secret: process.env.APP_SECRET,
