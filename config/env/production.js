5722fd1abf8875339027d2b7ed0ae3079656f5f0
ntin90 <nttin@misfit.com>
2017-03-13 22:35:25 +0700

    use postgres on heroku

--- config/env/production.js
+++ config/env/production.js
@@ -2,5 +2,14 @@ module.exports = {
     log: { level: "info"},
     self_host: process.env.DOMAIN,
     app_key: process.env.APP_KEY,
-    app_secret: process.env.APP_SECRET
+    app_secret: process.env.APP_SECRET,
+    connection: {
+        localDiskDb: {
+            adapter: 'sails-postgresql',
+            host: process.env.POSTGRES_HOST,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASS,
+            database: process.env.POSTGRES_DB
+        }
+    }
 };
