3961a1b59586e44c98912a07daf56caca772ffb1
Canh Nguyen <nxcanh@fossil.com>
2017-05-03 17:48:47 +0700

    Add log to file

--- config/log.js
+++ config/log.js
@@ -9,6 +9,16 @@
  * For more information on the Sails logger, check out:
  * http://sailsjs.org/#!/documentation/concepts/Logging
  */
+var winston = require('winston');
+
+var customLogger = new winston.Logger({
+  transports: [
+    new(winston.transports.File)({
+      level: 'debug',
+      filename: './application.log'
+    }),
+  ],
+});
 
 module.exports.log = {
 
@@ -24,6 +34,7 @@ module.exports.log = {
   *                                                                          *
   ***************************************************************************/
 
-  level: 'info'
+  level: 'info',
+  custom: customLogger
 
 };
